477fb83aded9e6bdfee18a52ad68ab5b6c86bc63
mrinal wahal <mrinal_w@rocketmail.com>
2019-07-21 16:23:11 +0530

    Added capacity to ensure SSH keys installed for the user

--- server.py
+++ server.py
@@ -5,20 +5,10 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 import os
-from main import configure
-import gogs_client
-
-path = "http://172.19.0.2:3000"
-api = gogs_client.GogsApi(path)
+from service import configure, authorize, ensureToken, ensureKeys, validUser, getRepos
 
 auth = None
 token = None
-
-def authorize(username, password):
-    return gogs_client.UsernamePassword(username, password)
-
-def ensureToken(auth):
-    return api.ensure_token(auth, 'gin-proc')
 
 @app.route('/')
 def index(): return render_template('index.html')
@@ -28,10 +18,11 @@ def index(): return render_template('index.html')
 def login():
     if request.method == "POST":
         auth = authorize(username = request.json['username'], password = request.json['password'])
-        if not api.valid_authentication(auth): abort(400)
+        if not validUser(auth): abort(400)
         print("User {} logged in".format(request.json['username']))
         token = ensureToken(auth = auth)
         print('token ensured: {}'.format(token.name))
+        print('key ensured: {}'.format(ensureKeys(token)))
         return ("logged in", 200)
     else:
         abort(400)
@@ -42,14 +33,14 @@ def hook():
         repo = request.form['repo']
         workflowFile = request.form['workflowFile']
         backPushFile = request.form['backPushFile']
-        configure(path = path, repoName = repo, workflowFile = workflowFile, backPushFile = backPushFile, token = token, user = auth.username)
+        configure(repoName = repo, workflowFile = workflowFile, backPushFile = backPushFile, token = token, auth = auth)
         return render_template('hook.html', repo = repo, workflowFile = workflowFile, backPushfile = backPushFile, status = "Done")
     else:
         abort(400)    
 
 @app.route('/repos', methods=['GET'])
 def repos():
-    if request.method == "GET": print(api.get_user_repos(auth, auth.username))
+    if request.method == "GET": print(getRepos(auth, auth.username))
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000, host="127.0.0.1")
---
+++ service.py
@@ -0,0 +1,115 @@
+import requests
+import os
+import gogs_client
+
+from cryptography.hazmat.primitives import serialization as crypto_serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend as crypto_default_backend
+
+path = "http://172.19.0.2:3000"
+api = gogs_client.GogsApi(path)
+
+def authorize(username, password):
+    return gogs_client.UsernamePassword(username, password)
+
+def ensureToken(auth):
+    return api.ensure_token(auth, 'gin-proc')
+
+def ensureKeys(token):
+	response = requests.get(path + "/api/v1/user/keys", headers = {'Authorization': 'token ' + str(token.token)})
+	for keys in response.json():
+		if keys['title'] == 'gin_id_rsa': return 'gin_id_rsa'
+	
+	key = rsa.generate_private_key(
+    backend=crypto_default_backend(),
+    public_exponent=65537,
+    key_size=2048
+	)
+	private_key = key.private_bytes(
+    crypto_serialization.Encoding.PEM,
+    crypto_serialization.PrivateFormat.PKCS8,
+    crypto_serialization.NoEncryption())
+	public_key = key.public_key().public_bytes(
+    crypto_serialization.Encoding.OpenSSH,
+    crypto_serialization.PublicFormat.OpenSSH
+	)
+
+	os.chmod('/.ssh/gin_id_rsa', 0o777)
+	os.chmod('/.ssh/gin_id_rsa.pub', 0o777)
+	with open('/.ssh/gin_id_rsa', 'w+') as private_key_file: 
+		private_key_file.write(private_key.decode('utf-8'))
+	with open('/.ssh/gin_id_rsa.pub', 'w+') as public_key_file: 
+		public_key_file.write(public_key.decode('utf-8'))
+	
+	response = requests.post(path + "/api/v1/user/keys", 
+	headers = {'Authorization': 'token ' + str(token.token)},
+	data = {'title': 'gin_id_rsa', 'key': public_key}
+	)
+
+	return 'gin_id_rsa'
+
+def validUser(auth):
+	return api.valid_authentication(auth)
+
+def designWorkflow(filename, repoPath):
+	lines = []
+	with open('Snakefile.template', 'r+') as template:
+		for line in template.readlines():
+			if not '#Add-Files' in line: lines.append(line)
+			else:lines.append("SAMPLES = ['%s']" % filename)
+
+	template.close()
+	print("Files added to workflow: " + filename)
+
+	with open(repoPath + '/Snakefile', 'w+') as config: config.writelines(lines)
+	config.close()
+
+	print("Workflow written at " + repoPath + "/Snakefile")
+
+def designBackPush(filename, repoPath):
+	lines = []
+	with open('drone.template', 'r+') as template:
+		for line in template.readlines():
+			if not '# Break-Point' in line: lines.append(line)
+			else :lines.append("    - git checkout master %s" % filename)
+
+	template.close()
+	print("Files added for back-push: " + filename)
+
+	with open(repoPath + '/.drone.yml', 'w+') as config: config.writelines(lines)
+	config.close()
+
+	print("Configuration written at " + repoPath + "/.drone.yml")
+
+def getRepos(auth, user):
+	return api.get_user_repos(auth, user)
+
+def getRepoData(auth, user, repo):
+	return api.get_repo(auth, user, repo)
+
+	print('Repo {} fetched'.format(repo))
+
+def clone(repo):
+	clone_path = "clones/wahal-" + repo['name']
+	if not os.path.exists(clone_path): os.makedirs(clone_path)
+	os.system("git clone --depth=1 " + repo['clone_url'] + " " + clone_path)
+
+	print("Repo cloned at " + clone_path)
+	return clone_path
+
+def clean(path):
+	os.system("rm -rf " + path)
+	print("Repo cleaned")
+
+def push(repoPath):
+	os.system('cd ' + repoPath + ' && git add . && git commit -m "Updated workflow" && git push origin master')
+	print("Updates pushed")
+
+def configure(repoName, workflowFile, backPushFile, token, auth):
+	repo = getRepoData(auth, auth.username, repoName)
+	clone_path = clone(repo)
+	designWorkflow(workflowFile, clone_path)
+	designBackPush(backPushFile, clone_path)
+	push(clone_path)
+	clean(clone_path)
+	print('\n[-] Ability to push the new CI config is yet to be added.')
